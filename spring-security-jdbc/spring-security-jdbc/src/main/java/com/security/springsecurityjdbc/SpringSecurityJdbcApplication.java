6d2d973d7224a2cde4c206e2b861193614a02ce5
Nishant.Nishant <nishant.nishant@cognizant.com>
2019-11-14 06:49:40 +0530

    spring security jdbc demo

---
+++ spring-security-jdbc/spring-security-jdbc/src/main/java/com/security/springsecurityjdbc/SpringSecurityJdbcApplication.java
@@ -0,0 +1,15 @@
+package com.security.springsecurityjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@SpringBootApplication
+public class SpringSecurityJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJdbcApplication.class, args);
+	}
+
+}
