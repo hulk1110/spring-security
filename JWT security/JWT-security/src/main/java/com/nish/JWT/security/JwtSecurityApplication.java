abe9839a7cd4e7a7eac44540dd7d712e4af60147
unknown <nishantsayshellotou@gmail.com>
2021-11-23 18:43:24 +0530

    default sprng securty implementation

--- JWT security/JWT-security/src/main/java/com/nish/JWT/security/JwtSecurityApplication.java
+++ JWT security/JWT-security/src/main/java/com/nish/JWT/security/JwtSecurityApplication.java
@@ -1,7 +1,11 @@
 package com.nish.JWT.security;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+		import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
+		import org.springframework.cglib.proxy.NoOp;
+		import org.springframework.context.annotation.Bean;
+		import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+		import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class JwtSecurityApplication {
@@ -9,5 +13,8 @@ public class JwtSecurityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JwtSecurityApplication.class, args);
 	}
-
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return NoOpPasswordEncoder.getInstance();
+	}
 }
---
+++ JWT security/JWT-security/src/main/java/com/nish/JWT/security/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.nish.JWT.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "HELLO FROM BANGALORE !!";
+    }
+}
